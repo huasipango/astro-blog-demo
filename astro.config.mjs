2f04d646b9941975641bbdcab6782e46dbd1249d
Anthony Cabrera <anthony.cabrera@minu.mx>
2025-03-10 11:11:13 +0100

    Hotfix alias @utils reference.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
+import path from 'path';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@utils': path.resolve('./src/utils')
+      }
+    }
   },
 
   integrations: [mdx()],
